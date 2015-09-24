6b0fbe686db70540a3d2ad4b04611512a473d6fa
Devon Weller <devon@tokenly.co>
2015-09-23 20:19:18 -0500

    refactored to support both keen and slack events

--- src/Tokenly/LaravelKeenEvents/KeenEvents.php
+++ src/Tokenly/LaravelKeenEvents/KeenEvents.php
@@ -13,17 +13,58 @@ class KeenEvents {
         $this->active           = $active;
     }
 
-    public function send($collection, $event) {
-        if (!$this->active) { return; }
-
-        $this->sendEventToBeanstalkQueue($collection, $event);
+    public function isActive() {
+        return $this->active;
     }
 
-    protected function sendEventToBeanstalkQueue($collection, $event) {
+    public function sendKeenEvent($collection, $event) {
+        if (!$this->active) { return; }
+
+        $this->sendEventToBeanstalkQueue($event, [
+            'jobType'    => 'keen',
+            'collection' => $collection,
+        ]);
+    }
+
+    public function sendSlackEvent($data_or_title, $text_or_fields) {
+        if (!$this->active) { return; }
+
+        if (is_array($text_or_fields)) {
+            $fields = $text_or_fields;
+        } else {
+            $fields = [['title' => 'Description', 'value' => $text_or_fields]];
+        }
+        $fields = $this->normalizeFields($fields);
+
+        if (is_array($data_or_title)) {
+            $event = $data_or_title;
+        } else {
+            $event = ['title' => $data_or_title];
+        }
+
+        $event['fields'] = $fields;
+
+        if (!isset($event['color'])) { $event['color'] = 'good'; }
+
+        $this->sendEventToBeanstalkQueue($event, [
+            'jobType'    => 'slack',
+        ]);
+    }
+
+    protected function normalizeFields($fields_in) {
+        $fields_out = [];
+        foreach($fields_in as $field) {
+            if (!isset($field['short'])) {
+                $field['short'] = (strlen($field['value']) < 25);
+            }
+            $fields_out[] = $field;
+        }
+        return $fields_out;
+    }
+
+    protected function sendEventToBeanstalkQueue($event, $meta) {
         $entry = [
-            'meta' => [
-                'collection' => $collection,
-            ],
+            'meta' => $meta,
             'data' => $event,
         ];
 
